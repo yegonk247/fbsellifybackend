550f7ef7b9fb0ac3836bed9ddcba290981f5b817
Yegon <meshcode75@gmail.com>
2025-11-04 21:20:07 +0300

    not connecting to database

--- databaseconnection.js
+++ databaseconnection.js
@@ -10,7 +10,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306 //
 };
 
 // SQL to create the license_keys table if it doesn't exist
